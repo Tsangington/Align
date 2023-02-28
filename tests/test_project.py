6cf354a755dcd5697731276e3fe87ad426747a77
Tsangington <harry.tsang@code.berlin>
2023-02-28 16:02:24 +0100

    Change SS2023 to Align  - Changed Main filename from SS2023  - Changed Align and ss2023_app to app

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,4 +1,4 @@
 def test_landing(client):
     response = client.get("/")
     assert b"<h1> Landing Page </h1>" in response.data
-    assert b"<title> ss2023 </title>" in response.data
+    assert b"<title> Align </title>" in response.data
